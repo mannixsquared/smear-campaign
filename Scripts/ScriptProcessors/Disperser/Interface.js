e12b504f25c460cda2a6d0981a8330508dd8f080
John Mannix <1419878+johnmannix@users.noreply.github.com>
2025-04-11 08:55:51 +0100

    Initial commit

---
+++ Scripts/ScriptProcessors/Disperser/Interface.js
@@ -0,0 +1,27 @@
+ include("LookAndFeel.js");
+ 
+ Content.makeFrontInterface(700, 200);
+ 
+ const var KnobPanel = Content.getComponent("KnobPanel");
+ KnobPanel.setLocalLookAndFeel(laf);
+ function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
